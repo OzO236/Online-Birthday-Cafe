71cf2d27e3db69a783caf70841138e3c0703a14a
jjjen23 <jelly2323@naver.com>
2024-02-20 20:34:07 +0900

    jen: css.module로 변경 및 적용

--- src/Pages/DetailLike.js
+++ src/Pages/DetailLike.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../style/detailLike.css'
+import styles from '../style/detailLike.module.css'
 import sirenwb from '../asset/sirenwb.png'
 import memorieswb from '../asset/memorieswb.png'
 import getaguitarwb from '../asset/getaguitarwb.png'
@@ -10,35 +10,35 @@ import staricon from '../asset/staricon.png'
 function DetailLike() {
   return(
     <div>
-      <div className='total_detaillke'>
-        <div className="container_detaillke">
-          <div className="title_detaillke">Which Wonbin do you like <br/> the most?</div>
-          <div className='p1'><p>최애 원빈이의 컨셉 하나를 선택해주세요.</p></div>
-          <div className='p2'><p>※하나만 선택 가능하며 한 번 누르면 다시 되돌릴 수 없습니다.</p></div>
+      <div className={styles.total}>
+        <div className={styles.container}>
+          <div className={styles.title}>Which Wonbin do you like <br/> the most?</div>
+          <div className={styles.p1}><p>최애 원빈이의 컨셉 하나를 선택해주세요.</p></div>
+          <div className={styles.p2}><p>※하나만 선택 가능하며 한 번 누르면 다시 되돌릴 수 없습니다.</p></div>
 
-          <div className='cardbox'>
-            <div className='box box1'>
+          <div className={styles.cardbox}>
+            <div className={`${styles.box} ${styles.box1}`}>
               <span>siren</span>
-              <img className='sirenimg' src={sirenwb} alt="sirenimg"/>
+              <img className={styles.sirenimg} src={sirenwb} alt="sirenimg"/>
             </div>
-            <div className='box box2'>
+            <div className={`${styles.box} ${styles.box2}`}>
               <span>memories</span>
-              <img className='memoriesimg' src={memorieswb} alt="memoriesimg"/>
+              <img className={styles.memoriesimg} src={memorieswb} alt="memoriesimg"/>
             </div>
-            <div className='box box3'>
+            <div className={`${styles.box} ${styles.box3}`}>
               <span>get a guitar</span>
-              <img className='getaguitarimg' src={getaguitarwb} alt="getaguitarimg"/>
+              <img className={styles.getaguitarimg} src={getaguitarwb} alt="getaguitarimg"/>
             </div>
-            <div className='box box4'>
+            <div className={`${styles.box} ${styles.box4}`}>
               <span>talk saxy</span>
-              <img className='talksaxyimg' src={talksaxywb} alt="talksaxyimg"/>
+              <img className={styles.talksaxyimg} src={talksaxywb} alt="talksaxyimg"/>
             </div>
-            <div className='box box5'>
+            <div className={`${styles.box} ${styles.box5}`}>
               <span>love 119</span>
-              <img className='love119img' src={love119wb} alt="love119img"/>
+              <img className={styles.love119img} src={love119wb} alt="love119img"/>
             </div>
-            <div className='box box6'>
-              <img className='staricon' src={staricon} alt="staricon"/>
+            <div className={`${styles.box} ${styles.box6}`}>
+              <img className={styles.staricon} src={staricon} alt="staricon"/>
             </div>
           </div>
 
--- src/Pages/DetailMap.js
+++ src/Pages/DetailMap.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import '../style/detailMap.css'
+import styles from '../style/detailMap.module.css'
 import riizebarcode from '../asset/riizebarcode.png'
 
 function DetailMap() {
-  return(
+  return (
     <div>
-      <div className='total_detailmap'>
-        <div className="container_detailmap"> 
-          <span className="star star1">★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>        
-          <div className='title_detailmap'>Wonbin's birthday <br/> cafe map </div>
-          <span className="star star2">★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>
-          <div className='rectangle'/>
-          <span className="star star3">★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>
-          <div className='textcontainer'>
-            <div className='flexcontainer'>
-              <span className='text'>국내에 있는 원빈 생일 카페 한정으로 지도에 표시됩니다.</span>
-              <span className='text'>지도 외에 다른 카페가 있다면 다음의 메일로 알려주시면 감사하겠습니다.</span>
-              <span className='text'>yeonju9623@naver.com</span>
+      <div className={styles.total}>
+        <div className={styles.container}> 
+          <span className={`${styles.star} ${styles.star1}`}>★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>        
+          <div className={styles.title}>Wonbin's birthday <br/> cafe map </div>
+          <span className={`${styles.star} ${styles.star2}`}>★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>
+          <div className={styles.rectangle}></div>
+          <span className={`${styles.star} ${styles.star3}`}>★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆ - - ★ - - ☆</span>
+          <div className={styles.textcontainer}>
+            <div className={styles.flexcontainer}>
+              <span className={styles.text}>국내에 있는 원빈 생일 카페 한정으로 지도에 표시됩니다.</span>
+              <span className={styles.text}>지도 외에 다른 카페가 있다면 다음의 메일로 알려주시면 감사하겠습니다.</span>
+              <span className={styles.text}>yeonju9623@naver.com</span>
             </div>
           </div>
-          <img className='barcode' src={riizebarcode} alt="barcode"/>
+          <img className={styles.barcode} src={riizebarcode} alt="barcode"/>
         </div>
-
       </div>
     </div>
-  )
-};
+  );
+}
+
 
 export default DetailMap
